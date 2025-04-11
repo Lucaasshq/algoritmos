df8e5bd5dc3469aa21a72f138ad37c1255d3f633
Lucaasshq <lucashenriquebarros2016@gmail.com>
2025-04-11 14:58:02 -0300

    lista completa

--- 1_Exercicios/10_.py
+++ 1_Exercicios/10_.py
@@ -4,3 +4,26 @@
 #   maior soma é 72, que é a soma dos elementos entre os índices 3 e 6 : 20 + 50 + −1 + 3.
 #                         10 5 -17 20 50 -1 3 -30 10
 #   Escreva um algoritmo que retorne o valor da Sub-lista contígua de soma máxima.
+
+
+#! Algoritmo de Kadane
+
+def kadane(a:list):
+  somaMaxima = somaAtual = a[0]
+
+  for i in a[1:]:
+    if somaAtual + i > i:
+      somaAtual += i
+    else:
+      somaAtual = i
+
+    if somaAtual > somaMaxima:
+      somaMaxima = somaAtual
+
+  return somaMaxima
+
+
+a: list = [10, 5, -17, 20, 50, -1, 3, -30, 10]
+
+result:int = kadane(a)
+print(result)
