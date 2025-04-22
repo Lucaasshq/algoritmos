b1296ce61df9215a117cca0165be14db0dac7db9
Lucaasshq <lucashenriquebarros2016@gmail.com>
2025-04-22 19:24:13 -0300

    .

--- 2_Exercicios/8/8_.cpp
+++ 2_Exercicios/8/8_.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 using namespace std;
 
+
+int calcularMDC(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
 int main() {
-    
-    cout << "ola";
+    int a, b;
+    cout << "Digite dois numeros naturais positivos: ";
+    cin >> a >> b;
+
+    if (a <= 0 || b <= 0) {
+        cout << "Os numeros devem ser naturais positivos!" << endl;
+        return 1;
+    }
+
+    int mdc = calcularMDC(a, b);
+    cout << "O MDC de " << a << " e " << b << " eh: " << mdc << endl;
 
     return 0;
 }
