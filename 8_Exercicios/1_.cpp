5fe7231ff57a2a0054a175aa778c7de9a037e166
Lucas H B Medeiros <120293165+Lucaasshq@users.noreply.github.com>
2025-06-07 21:26:43 +0000

    .

---
+++ 8_Exercicios/1_.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <chrono>
+const int INITIAL_CAPACITY = 100, INCREASE_CAPACITY = 100;
+// Função para aumentar a capacidade do array
+int* increase_capacity(int* data, int& capacity, int& size) {
+    int* new_array = new int[capacity + INCREASE_CAPACITY];
+
+    for (int i = 0; i < size; ++i)
+        new_array[i] = data[i];
+
+    delete[] data;  // libera a memória antiga
+    
+    capacity = capacity + INCREASE_CAPACITY;
+    return new_array;
+}
+// Função para inserir valor no final (como push_back)
+int* push_back(int* data, int& capacity, int& size, int value) {
+    if (size == capacity)
+        data = increase_capacity(data, capacity, size);
+
+    data[size++] = value;
+    return data;
+}
+int main() {
+    int* data;
+    int size = 0, capacity = INITIAL_CAPACITY, x;
+
+    auto beg = std::chrono::high_resolution_clock::now();
+
+    data = new int[capacity];  // alocação inicial
+
+    while (std::cin >> x, x != -1)  // lê até digitar -1
+        data = push_back(data, capacity, size, x);
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Quantidade de números lidos: " << size << std::endl;
+    std::cout << "Tamanho do vetor: " << capacity << std::endl;
+
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - beg);
+    std::cerr << "Tempo de processamento: " << duration.count() << " microseconds(s)" << std::endl;
+    delete[] data;  // libera a memória alocada
+    return 0;
+}
